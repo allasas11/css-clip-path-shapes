0376a32f0c8a3bab107da843d090713a665e035e
Alfonsas <alfonsasalubeckas@gmail.com>
2025-09-18 12:06:10 +0300

    feat: add home page with clip-path introduction

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import styles from "./HomePage.module.scss";
+
+function Home() {
+  return (
+    <div className={styles.homeHeader}>
+      <h1>Home: CSS Clip-Path Shapes</h1>
+      <p>Explore various CSS clip-path shapes and their implementations.</p>
+    </div>
+  );
+}
+
+export default Home;
