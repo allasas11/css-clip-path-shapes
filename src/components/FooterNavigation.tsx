0d4bfb4df4f6c292c9992b497bfd1893483c64a1
Alfonsas <alfonsasalubeckas@gmail.com>
2025-09-18 15:35:21 +0300

    default navs implemented

--- src/components/FooterNavigation.tsx
+++ src/components/FooterNavigation.tsx
@@ -1,7 +1,11 @@
+import styles from "./FooterNavigation.module.scss";
+
 function FooterNavigation() {
   return (
-    <footer>
-      Footer Navigation
+    <footer className={styles.footerNavigation}>
+      <div className={styles.footerContent}>
+        <p className={styles.footerText}>@All Rights Reserved</p>
+      </div>
     </footer>
   );
 }
--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -1,8 +1,21 @@
+import styles from "./MainNavigation.module.scss";
+
 function MainNavigation() {
   return (
-    <nav>
-      {/* Navigation content */}
-      Main Navigation
+    <nav className={styles.mainNavigation}>
+      <div className={styles.logoContainer}>
+        Logo
+      </div>
+      <div className={styles.navigationContainer}>
+        <ul className={styles.navigationList}>
+          <li className={styles.navigationItem}>
+            <a 
+              className={styles.navigationLink} 
+              href="/">Home
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
